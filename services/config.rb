53d56cc23b966ce85be9904b875d3c8cd164b057
Paul Allen <paul.allen@lithium.com>
2014-09-11 16:35:32 -0500

    updating

--- services/config.rb
+++ services/config.rb
@@ -1,4 +1,3 @@
-
 ## This file was automatically generated using the CloudCoreo CLI
 ##
 ## This config.rb file exists to create and maintain services not related to compute.
@@ -11,3 +10,59 @@
 ## end
 ##
 
+coreo_aws_ec2_securityGroups "${JENKINS_NAME}" do
+  action :sustain
+  description "Jenkins security group"
+  vpc "${VPC_NAME}"
+  allows [ 
+          { 
+            :direction => :ingress,
+            :protocol => :tcp,
+            :ports => ${JENKINS_INGRESS_PORTS},
+            :cidrs => ${JENKINS_INGRESS_CIDRS}
+          }
+    ]
+end
+
+coreo_aws_iam_policy "${JENKINS_NAME}" do
+  action :sustain
+  policy_name "AllowS3Backup"
+  policy_document <<-EOH
+{
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Resource": [
+          "arn:aws:s3:::${BACKUP_BUCKET}/${REGION}/${ENV}/jenkins/${JENKINS_NAME}",
+          "arn:aws:s3:::${BACKUP_BUCKET}/${REGION}/${ENV}/jenkins/${JENKINS_NAME}/*
+      ],
+      "Action": [ 
+          "s3:*"
+      ]
+    }
+  ]
+}
+EOH
+end
+
+coreo_aws_iam_instance_profile "${JENKINS_NAME}" do
+  action :sustain
+  policies ["${JENKINS_NAME}"]
+end
+
+coreo_aws_ec2_instance "${JENKINS_NAME}" do
+  action :define
+  image_id "${JENKINS_AMI}"
+  size "${JENKINS_SIZE}"
+  security_groups ["${JENKINS_NAME}"]
+  associate_public_ip true
+  role "${JENKINS_NAME}"
+end
+
+coreo_aws_ec2_autoscaling "${JENKINS_NAME}" do
+  action :sustain 
+  minimum 1
+  maximum 1
+  server_definition "${JENKINS_NAME}"
+  subnet "${PRIVATE_SUBNET_NAME}"
+end
