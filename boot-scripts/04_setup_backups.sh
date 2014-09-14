b49ffc529c8d50dbb9ec6551e970c8a0caa0f16f
Paul Allen <paul.allen@lithium.com>
2014-09-14 14:22:34 -0500

    adding backup script to the crontab

---
+++ boot-scripts/04_setup_backups.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if ! rpm -qa | grep -q cloudcoreo-directory-backup; 
+    yum install -y cloudcoreo-directory-backup
+fi
+
+MY_AZ="$(curl -sL 169.254.169.254/latest/meta-data/placement/availability-zone)"
+MY_REGION="$(echo ${MY_AZ%?})"
+
+echo "0 * * * * python cloudcoreo-directory-backup.py --s3-backup-region ${BACKUP_BUCKET_REGION} --s3-backup-bucket ${BACKUP_BUCKET} --s3-prefix ${MY_REGION}/jenkins/${ENV}/${JENKINS_NAME} --directory /var/lib/jenkins --dump-dir /tmp" | crontab
+
+echo "0 * * * * ps -fwwC python | grep -q cloudcoreo-directory-backup || { cd /opt/; nohup python cloudcoreo-directory-backup.py --s3-backup-region ${BACKUP_BUCKET_REGION} --s3-backup-bucket ${BACKUP_BUCKET} --s3-prefix ${MY_REGION}/jenkins/${ENV}/${JENKINS_NAME} --directory /var/lib/jenkins --dump-dir /tmp & }" | crontab
