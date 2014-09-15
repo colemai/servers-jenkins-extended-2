6b824ab045533cfaf5dc8add2724c031bcf48076
Paul Allen <paul.allen@lithium.com>
2014-09-14 22:37:20 -0500

    parameterizing the cron entry

--- boot-scripts/04_setup_backups.sh
+++ boot-scripts/04_setup_backups.sh
@@ -7,4 +7,4 @@ fi
 MY_AZ="$(curl -sL 169.254.169.254/latest/meta-data/placement/availability-zone)"
 MY_REGION="$(echo ${MY_AZ%?})"
 
-echo "*/15 * * * * ps -fwwC python | grep -q cloudcoreo-directory-backup || { cd /opt/; nohup python cloudcoreo-directory-backup.py --s3-backup-region ${BACKUP_BUCKET_REGION} --s3-backup-bucket ${BACKUP_BUCKET} --s3-prefix ${MY_REGION}/jenkins/${ENV}/${JENKINS_NAME} --directory /var/lib/jenkins --dump-dir /tmp & }" | crontab
+echo "${JENKINS_BACKUP_CRON} ps -fwwC python | grep -q cloudcoreo-directory-backup || { cd /opt/; nohup python cloudcoreo-directory-backup.py --s3-backup-region ${BACKUP_BUCKET_REGION} --s3-backup-bucket ${BACKUP_BUCKET} --s3-prefix ${MY_REGION}/jenkins/${ENV}/${JENKINS_NAME} --directory /var/lib/jenkins --dump-dir /tmp & }" | crontab
