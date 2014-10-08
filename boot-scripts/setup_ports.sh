850c23f3f7a6cc39be2bbc2d0e0e9ba98e26de6b
Paul Allen <paul.allen@lithium.com>
2014-10-08 12:23:13 -0500

    save the iptables

--- boot-scripts/setup_ports.sh
+++ boot-scripts/setup_ports.sh
@@ -3,4 +3,5 @@
 ## this will ensure we can access jenkins on port 80
 if ! iptables -t nat -L | grep -q '^REDIRECT.*8080'; then
     iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
+    service iptables save
 fi
