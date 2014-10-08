0a800134bab657836eb797c12fc9d3ba17e79476
Paul Allen <paul.allen@lithium.com>
2014-10-07 22:16:47 -0500

    removing tmp ami

--- services/config.rb
+++ services/config.rb
@@ -113,7 +113,7 @@ end
 
 coreo_aws_ec2_instance "${JENKINS_NAME}" do
   action :define
-  image_id "ami-12345678"
+  image_id "${JENKINS_AMI}"
   size "${JENKINS_SIZE}"
   security_groups ["${JENKINS_NAME}"]
   role "${JENKINS_NAME}"
