1c048c37a1ae61ad28ed568ba935f622f99be1eb
colemai <colemai@tcd.ie>
2016-11-30 15:15:23 -0800

    Added vpc subnet to config.rb

--- services/config.rb
+++ services/config.rb
@@ -47,6 +47,13 @@ coreo_aws_iam_policy "${JENKINS_NAME}-route53" do
 EOH
 end
 
+coreo_aws_vpc_subnet "{$JENKINS_NAME}-subnet" do
+  action :sustain
+  number_of_zones 3
+  percent_of_vpc_allocated 25
+  vpc "${JENKINS_NAME}-vpc"
+end
+
 coreo_aws_iam_policy "${JENKINS_NAME}-s3" do
   action :sustain
   policy_name "AllowJenkinsS3Backup"
