52429eda4763225f843e17ff3ac075cf8a138968
colemai <colemai@tcd.ie>
2016-12-01 11:34:30 -0800

    routeable added

--- services/config.rb
+++ services/config.rb
@@ -51,17 +51,14 @@ coreo_aws_vpc_subnet "${JENKINS_NAME}-subneta" do
   action :sustain
   vpc "${JENKINS_NAME}-vpc"
   percent_of_vpc_allocated 25
+  route_table "${JENKINS_NAME}-routetable"
 end
 
-# coreo_aws_vpc_routetable "test-routetable-aa" do
-#   action :sustain
-#   vpc "test-vpc-aa"
-#   number_of_tables 3
-#   tags [
-#            "Paul=Allen",
-#            "dev=true"
-#        ]
-# end
+coreo_aws_vpc_routetable "${JENKINS_NAME}-routetable" do
+  action :sustain
+  vpc "${JENKINS_NAME}-vpc"
+  number_of_tables 3
+end
 
 coreo_aws_iam_policy "${JENKINS_NAME}-s3" do
   action :sustain
